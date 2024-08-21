d907e0dcbfbc58c1093aeb700eab44022718f48e
jaeheon jeong <huchehuche@naver.com>
2024-08-21 10:24:50 +0900

    11

---
+++ src/component_manage/ListRender.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// 리스트 렌더링 :
+// - jsx 내부에서 반복적으로 렌더링하는  요소에 대하여 반복 처리하는 방법
+// - jsx 내부에서는 for,while과 같은 반복 구문을 사용 할 수 없음
+// - list의 각종 메서드 활용
+
+export default function ListRender() {
+    return (
+        <div>ListRender</div>
+    )
+}
